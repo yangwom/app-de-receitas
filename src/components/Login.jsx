1f955736d811df9a15c8f7e6881fbbfe18418480
yang vieira <yangflix3@gmail.com>
2022-03-08 09:39:46 -0400

    requisito 08

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -12,7 +12,7 @@ function Login() {
     localStorage.setItem('mealsToken', 1);
     localStorage.setItem('cocktailsToken', 1);
     localStorage.setItem('user', JSON.stringify({ email }));
-    history.push('explore');
+    history.push('foods');
   };
 
   return (
