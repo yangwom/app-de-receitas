d30cfec35105eb8e389eebd4acb5db3f28022474
MatheusBarcellosDev <matheusbarcellos61@gmail.com>
2022-03-16 16:10:43 -0300

    Css

--- src/components/cardsDone/index.js
+++ src/components/cardsDone/index.js
@@ -18,36 +18,50 @@ export default function CardsDone({
   const [copy, setCopy] = useState(false);
   const typeVerification = type === 'food' ? nationality : alcoholicOrNot;
   return (
-    <li key={ id }>
-      <Link key={ id } to={ `/${type}s/${id}` }>
-        <img data-testid={ `${index}-horizontal-image` } src={ img } alt="" />
-        <p data-testid={ `${index}-horizontal-name` }>{`Name: ${name}`}</p>
-      </Link>
-      <p data-testid={ `${index}-horizontal-top-text` }>
-        {`Category:  ${typeVerification} - ${category}`}
-      </p>
-      <p data-testid={ `${index}-horizontal-done-date` }>{`Date: ${date}`}</p>
-      <span>
-        Tags:
-        {tags.map((tag) => (
-          <span key={ tag } data-testid={ `${index}-${tag}-horizontal-tag` }>
-            {` ${tag}`}
-          </span>
-        ))}
-      </span>
-      <button
-        onClick={ () => {
-          navigator.clipboard.writeText(`${window.location.origin}/${type}s/${id}`);
-          setCopy(true);
-        } }
-        src={ icon }
-        type="button"
-        data-testid={ `${index}-horizontal-share-btn` }
-      >
-        Share
-      </button>
-      { copy && <p>Link copied!</p>}
-    </li>
+    <div key={ id } className="container__cardFavorite">
+      <div className="cardFavorite__img">
+        <Link key={ id } to={ `/${type}s/${id}` }>
+          <img data-testid={ `${index}-horizontal-image` } src={ img } alt="" />
+        </Link>
+      </div>
+      <div className="cardFavorite__infos">
+        <Link key={ id } to={ `/${type}s/${id}` } className="cardFavorite__infos--name">
+          <p data-testid={ `${index}-horizontal-name` }>{`Name: ${name}`}</p>
+        </Link>
+        <p
+          data-testid={ `${index}-horizontal-top-text` }
+          className="cardFavorite__infos--category"
+        >
+          {`Category:  ${typeVerification} - ${category}`}
+        </p>
+        <p
+          data-testid={ `${index}-horizontal-done-date` }
+          className="cardFavorite__infos--category"
+        >
+          {`Date: ${date}`}
+
+        </p>
+        <span>
+          {tags.map((tag) => (
+            <span key={ tag } data-testid={ `${index}-${tag}-horizontal-tag` }>
+              {` ${tag}`}
+            </span>
+          ))}
+        </span>
+        <button
+          onClick={ () => {
+            navigator.clipboard.writeText(`${window.location.origin}/${type}s/${id}`);
+            setCopy(true);
+          } }
+          src={ icon }
+          type="button"
+          data-testid={ `${index}-horizontal-share-btn` }
+        >
+          Share
+        </button>
+        { copy && <p>Link copied!</p>}
+      </div>
+    </div>
   );
 }
 
